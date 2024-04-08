cbb3e4f1d9844f09d1587267f777e9d0d41dbc4c
LukeFromAfar <lukasz.gamvik@gmail.com>
2024-04-08 17:59:15 +0200

    Quotegenerator commit

--- js/quotegenerator.js
+++ js/quotegenerator.js
@@ -1,4 +1,28 @@
-const getQuote = async () => {
+let category = "money";
 
-}
+const getQuote = async () => {
+    
+    let category = document.getElementById("categoryInputField").value;
+    const url = 'https://api.api-ninjas.com/v1/quotes?category=' + category;
+    let headers = new Headers({
+    'X-Api-Key': 'kxtFWPMd9+e4d2TCEJdiXg==FM0sdZR1qb3ZEIz9',
+    'Content-Type': 'application/json'
+});
+
+fetch(url, { method: 'GET', headers: headers })
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("quote-ct").innerHTML = `${data[0].quote}`;
+        document.getElementById("author-ct").innerHTML = `${data[0].author}`;
+
+    })
+    .catch(error => console.error('Error:', error));
+};
+
+
+const genButton = document.getElementById("generateButton");
+
+genButton.addEventListener("click", () => {
+    getQuote();
+})
 
